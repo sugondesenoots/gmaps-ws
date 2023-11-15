16cd6d9fc0a506f223fafe6468eabbe905ec31d2
sugondesenoots <martinmangahas972005@gmail.com>
2023-11-16 01:12:10 +0800

    Check-In (Q1 Matrices DONE)
    Moving on to Q2.

---
+++ GMAPS_Oct_2023_Worksheets STUDENT/Assets/02_MATRICES_worksheet/Sonic/TestMatrix.cs
@@ -0,0 +1,17 @@
+// Uncomment this whole file.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMatrix : MonoBehaviour
+{
+    private HMatrix2D mat = new HMatrix2D();
+
+    void Start()
+    {
+        mat.SetIdentity();
+        mat.Print();
+    }
+
+}
